036c5b9f61fc75a3bab9f2bb55c30f3b583a1b3a
Anant-trip <ananttripathi705@gmail.com>
2026-04-07 17:00:09 +0530

    Update SecurityConfig.java

--- src/main/java/com/tyss/config/SecurityConfig.java
+++ src/main/java/com/tyss/config/SecurityConfig.java
@@ -1,3 +1,4 @@
+package com.tyss.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -6,6 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import jakarta.servlet.DispatcherType; // Use Jakarta for Spring Boot 3
+
 
 @Configuration
 @EnableWebSecurity
